299d1c51c351dbe4139ba97c0f3d4b708b783b35
rustutam <Rustam.Asbapov@urfu.me>
2023-06-25 21:00:34 +0500

    исправлен help

--- main.py
+++ main.py
@@ -5,19 +5,15 @@ import scanner
 
 
 def parse(args):
-    parser = argparse.ArgumentParser(description="Script for scanning ports on host. "
-                                                 "Allows TCP and UDP ports, and define protocol, using this port.")
+    parser = argparse.ArgumentParser()
 
     parser.add_argument('-t', dest='tcp', action='store_true',
-                        help='Scan tcp ports. '
-                             'If tcp and udp flags both are absent, '
-                             'scanner checks only tcp ports.')
+                        help='the program scan tcp ports.')
     parser.add_argument('-u', dest='udp', action='store_true',
-                        help='Scan udp ports. Require root user.')
+                        help='the program scan udp ports.')
     parser.add_argument('-p', '--ports', action='store', nargs=2, type=int,
-                        default=[1, 65535], help='Ports range to scan. '
-                                                 'By default scan all ports.')
-    parser.add_argument('host', action='store', help='Host for scanning')
+                        default=[1, 65535], help='Ports range to scan.')
+    parser.add_argument('host', action='store', help='host for scanning')
 
     args = parser.parse_args(args)
     if not args.tcp and not args.udp:
